0c5e29f177735d3c5bc8b88904bdb34a5e2cdd1a
Degaei <64739542+Degaei@users.noreply.github.com>
2020-12-04 22:43:38 +0300

    Add files via upload

---
+++ forcedpvpConfig.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
+using Terraria.ModLoader.Config.UI;
+using Terraria.UI;
+
+namespace forcedpvp
+{
+    public class forcedpvpConfigServer : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+
+		[Label("Disable PvP")]
+		[Tooltip("Disables PvP if enabled")]
+		public bool Active { get; set; }
+
+		[Label("Disable team changing")]
+		[Tooltip("Disables team changing if enabled")]
+		public bool Allowed { get; set; }
+
+
+		public override bool AcceptClientChanges(ModConfig pendingConfig, int whoAmI, ref string message)
+		{
+			if (Main.player[whoAmI].name != "Gnidolin" || Main.player[whoAmI].name != "Gratfor")
+			{
+				message = "Sorry, you are not allowed to change these settings.";
+				return false;
+			}
+			return true;
+		}
+
+	}
+}
